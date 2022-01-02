6a65a3f727d43e809854a979300cf24898fb952b
Henry Chu <henrytech@outlook.com>
2022-01-02 18:51:15 +0800

    refactor tests

--- redstone-kotlin/src/commonTest/kotlin/redstone/kotlin/io/OkioTest.kt
+++ redstone-kotlin/src/commonTest/kotlin/redstone/kotlin/io/OkioTest.kt
@@ -5,11 +5,9 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 @Test
-fun testContains() {
-    listOf("", "/", "\\aa").forEach { prefix ->
-        assertTrue("${prefix}aa/bb/cc".toPath() in "${prefix}aa/bb".toPath())
-        assertTrue("${prefix}aa/bb/cc/..".toPath() in "${prefix}aa/bb".toPath())
-        assertTrue("${prefix}aa/bb/cc/.././.".toPath() in "${prefix}aa/bb".toPath())
-        assertTrue("${prefix}aa/bb/cc/../..".toPath() !in "${prefix}aa/bb".toPath())
-    }
+fun testContains() = listOf("", "/", "\\aa").forEach { prefix ->
+    assertTrue("${prefix}aa/bb/cc".toPath() in "${prefix}aa/bb".toPath())
+    assertTrue("${prefix}aa/bb/cc/..".toPath() in "${prefix}aa/bb".toPath())
+    assertTrue("${prefix}aa/bb/cc/.././.".toPath() in "${prefix}aa/bb".toPath())
+    assertTrue("${prefix}aa/bb/cc/../..".toPath() !in "${prefix}aa/bb".toPath())
 }
